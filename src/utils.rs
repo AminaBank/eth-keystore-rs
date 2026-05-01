81644fcf8f848c0c35f745f972677af2a6778b2f
Giovanni Napoli <giovanni.napoli@aminagroup.com>
2026-05-01 16:45:17 +0200

    fix `address_from_pk` for `geth-compat` feature

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "geth-compat")]
 pub mod geth_compat {
     use ethereum_types::H160 as Address;
-    use k256::{ecdsa::SigningKey, elliptic_curve::sec1::ToEncodedPoint, PublicKey};
+    use k256::ecdsa::SigningKey;
     use sha3::{Digest, Keccak256};
 
     use crate::KeystoreError;
@@ -11,8 +11,8 @@ pub mod geth_compat {
     where
         S: AsRef<[u8]>,
     {
-        let secret_key = SigningKey::from_bytes(pk.as_ref())?;
-        let public_key = PublicKey::from(&secret_key.verifying_key());
+        let secret_key = SigningKey::from_bytes(pk.as_ref().into())?;
+        let public_key = secret_key.verifying_key();
         let public_key = public_key.to_encoded_point(/* compress = */ false);
         let public_key = public_key.as_bytes();
         debug_assert_eq!(public_key[0], 0x04);
